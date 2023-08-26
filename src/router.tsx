949c81a827bb9ab0e2934cd0786382686b09da32
Rogerio Pontual <rppontual@gmail.com>
2023-08-26 09:08:43 +0100

    Add Links menu

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,8 @@
 import {
   createRoutesFromElements,
-  createBrowserRouter
+  createBrowserRouter,
+  Outlet, 
+  Link 
 } from "react-router-dom"
 import { Route } from "react-router"
 
@@ -12,12 +14,49 @@ const FallbackRoute = () => {
   return (<div>Not found</div>)
 }
 
+const Home = () => {
+  return (<div><h2>Home</h2></div>)
+}
+
+const Layout = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/nfts">My NFTS</Link>
+          </li>
+          <li>
+            <Link to="/auctions">My Auctions</Link>
+          </li>
+          <li>
+            <Link to="/all-auctions">All Auctions</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <hr />
+
+      {/* An <Outlet> renders whatever child route is currently active,
+          so you can think about this <Outlet> as a placeholder for
+          the child routes we defined above. */}
+      <Outlet />
+    </div>
+  );
+}
+
 export default createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" errorElement={<FallbackRoute />}>
-      <Route index element={<AllAuctionsPage />} />
-      <Route path="/auctions" element={<MyAuctionsPage />} />
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
       <Route path="/nfts" element={<MyNftsPage />} />
+      <Route path="/auctions" element={<MyAuctionsPage />} />
+      <Route path="/all-auctions" element={<AllAuctionsPage />} />
+      <Route path="*" element={<FallbackRoute />} />
     </Route>
   )
 )
+
